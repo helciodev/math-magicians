1da690d336dc8789d02797ee7b366e841d858070
helciodev <helcioandre@live.com>
2021-07-29 16:17:54 +0100

    Add initial files and configurations

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <h1>hello world!</h1>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
