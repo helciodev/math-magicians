b92bdab8e04de166b046df9718418233a9221962
helciodev <helcioandre@live.com>
2021-08-02 12:26:04 +0100

    Import calculate to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+/* eslint-disable no-unused-vars */
+import calculate from '../logic/calculate';
 
 function App() {
   return (
