a0c6d8d8f68e7f99261d71db5515cbd5030f646b
helciodev <helcioandre@live.com>
2021-08-17 19:28:04 +0100

    Add Error component

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,18 @@ import Navbar from './Navbar';
 import Calculator from './Calculator';
 import Home from './Home';
 import Quote from './Quote';
+import Error from './Error';
 
 function App() {
   return (
     <>
-
-      {/*
-  I will add a nav element with respective links
-  I will wrap everything inside a main element */}
       <Navbar />
       <main className="calculator-container">
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/calculator" component={Calculator} />
           <Route path="/quote" component={Quote} />
+          <Route component={Error} />
         </Switch>
 
       </main>
---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Error() {
+  return (
+    <>
+      <h1>😵 page not found!</h1>
+    </>
+  );
+}
+
+export default Error;
