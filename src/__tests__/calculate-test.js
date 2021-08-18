172227fc2ce1efc60a8f638d07d1688c64e89180
helciodev <helcioandre@live.com>
2021-08-18 15:40:42 +0100

    Add test case for AC button

--- src/__tests__/calculate-test.js
+++ src/__tests__/calculate-test.js
@@ -2,22 +2,31 @@ import calculate from '../logic/calculate';
 
 describe('calculate', () => {
   describe('button +', () => {
-    const [total, operation, next] = calculate([1, '', 2], '+');
-    it('sums total and next', () => {
-      expect(total).toBe(1);
-      expect(next).toEqual(2);
+    const [total, operation, next] = calculate(['1', '', '2'], '+');
+    it('returns total, operation and next', () => {
+      expect(total).toBe('1');
+      expect(next).toEqual('2');
       expect(operation).toEqual('+');
     });
   });
 
   describe('button +/-', () => {
-    let [total] = calculate([1, '', ''], '+/-');
+    let [total] = calculate(['1', '', ''], '+/-');
     it('transforms total into a negative integer', () => {
       expect(total).toBe(-1);
     });
     it('transforms total into a positive integer', () => {
-      [total] = calculate([-1, '', ''], '+/-');
+      [total] = calculate(['-1', '', ''], '+/-');
       expect(total).toBe(1);
+    });
+  });
+
+  describe('button AC', () => {
+    const [total, operation, next] = calculate(['1', '', '2'], 'AC');
+    it('it return total, operation and next as empty strings', () => {
+      expect(total).toEqual('');
+      expect(operation).toEqual('');
+      expect(next).toEqual('');
     });
   });
 });
