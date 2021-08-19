eb96d67c43ce9f6f96f7cb0c53d083fb50fa1ba2
helciodev <helcioandre@live.com>
2021-08-19 09:13:52 +0100

    Fix typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <>
-      <h1 className="greetings">Welcome ro our page</h1>
+      <h1 className="greetings">Welcome to our page</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aperiam nulla cumque </p>
     </>
   );
